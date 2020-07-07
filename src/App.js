5287c5c5703d9bb705a1339a062e3a6a14c654e3
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-07-08 00:06:07 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import Ipad from "./components/Ipad/Ipad";
 
 function App() {
   return (
     <div className="App">
       <h1>Music app</h1>
+      <div className="ipad-section">
+        <Ipad />
+      </div>
     </div>
   );
 }
---
+++ src/components/Ipad/Ipad.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Ipad.css";
+const Ipad = () => {
+  return (
+    <div>
+      <div className="main-content">
+        <div className="music-container">
+          <div className="album-img">img</div>
+          <div className="album-song">song</div>
+        </div>
+        <div className="top-music-list">top chart</div>
+      </div>
+    </div>
+  );
+};
+export default Ipad;
